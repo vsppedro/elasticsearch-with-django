af716bde96fe21d1c4bdb1d77241b4eae9138040
Pedro Paiva <pedrovsp109@gmail.com>
2017-12-29 16:19:14 -0300

    Connecting ElasticSearch with Django

---
+++ elasticsearchproject/elasticsearchapp/search.py
@@ -0,0 +1,13 @@
+from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl import DocType, Text, Date
+
+connections.create_connection()
+
+class BlogPostIndex(DocType):
+    author = Text()
+    posted_date = Date()
+    title = Text()
+    text = Text()
+
+    class Meta:
+        index = 'blogpost-index'
